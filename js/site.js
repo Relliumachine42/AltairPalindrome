626f7ead93bf878be0e53527976be04d50cbb136
Rellium <relliumachine42@gmail.com>
2023-07-26 09:10:48 -0400

    basically works

--- js/site.js
+++ js/site.js
@@ -1,13 +1,30 @@
-function displayMessage() {
-    let inputBox = document.getElementById('message');
-    
-    let message = inputBox.value;
+function getValues() {
+    let userString = document.getElementById('userString').value;
+    let finalAnswer = checkForPalindrome(userString);
+    displayString(finalAnswer);
+}
 
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'App Name',
-            text: message
-        }
-    );
+function checkForPalindrome(input) {
+    let revString = '';
+    let answer = 'no';
+    for (let i = input.length - 1; i >= 0; i--) {
+        revString += input[i];
+    }
+    if (revString == input) {
+        answer = 'yes';
+    }
+    let output = [revString, answer];
+    return output;
+}
+
+function displayString(output) {
+    document.getElementById('results').textContent = output[0];
+    if (output[1] == 'yes') {
+        document.getElementById('result').textContent = 'Sure is';
+        document.getElementById('alert').classList.add('alert-success');
+    } else {   
+        document.getElementById('result').textContent = 'Not a chance';
+        document.getElementById('alert').classList.add('alert-danger');
+    }
+    document.getElementById('alert').classList.remove('invisible');
 }
